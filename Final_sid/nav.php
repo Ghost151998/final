ddceea909c23ccb3ac09888783698d2155a82637
Ghost151998 <lucky.barhi@gmail.com>
2018-03-25 07:55:15 +0530

    onhover no background fixed

--- Final_sid/nav.php
+++ Final_sid/nav.php
@@ -6,7 +6,13 @@
 <script src="bootstrap-hover-dropdown.js"></script>
 
 <style>
-    
+  /*  body{
+      background:url("4.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
+       background-attachment: fixed;  
+         
+    } */
 </style>
 
  <script>
@@ -15,7 +21,7 @@
     })
  </script>
 
-<nav class="navbar  navbar-inverse" data-spy="affix"  >
+<nav class="navbar  navbar-inverse navbar-custom" data-spy="affix"  >
     <div class="container-fluid fix">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" data-spy="affix">
         <span class="icon-bar"></span>
@@ -24,11 +30,7 @@
       </button>
       <a class="navbar-brand col-sm-1" href="#footer">Logo</a>
       <?php 
-        
-        $dir = explode("/", $_SERVER["REQUEST_URI"]);
-        $last = end($dir);
-        $common_uri = str_replace($last, "", $_SERVER["REQUEST_URI"]);
-        
+        $common_uri = "/Final_sid/"; //Fill this with the current directory
         $current_uri = str_replace( $common_uri, "", $_SERVER["REQUEST_URI"]);
         //echo $current_uri;
       ?>
@@ -36,14 +38,16 @@
       <div class="navbar-collapse  collapse ">
         <ul class="nav navbar-nav">
             <li <?php if($current_uri == "user_home.php"){echo "class='active'";} ?> ><a href="user_home.php" id="home">Home</a></li>
-          <li class='dropdown <?php 
-              if( $current_uri == "items_list.php?category=books&book_sem=1" || $current_uri == "items_list.php?category=books&book_branch=cseit" || $current_uri == "items_list.php?category=books&book_branch=ece" ||  $current_uri == "items_list.php?category=books&book_branch=ee" ||  $current_uri == "items_list.php?category=books&book_branch=chem" ||  $current_uri == "items_list.php?category=books&book_branch=mechprod" ||  $current_uri == "items_list.php?category=books&book_branch=civ" ||  $current_uri == "items_list.php?category=books&book_branch=biot" ||  $current_uri == "items_list.php?category=books" )
+
+          <a href="#" class="dropdown-toggle js-activated">
+          <li class='dropdown' <?php 
+              if( $current_uri == "items_list.php?category=books&book_branch=cseit" ||  $current_uri == "items_list.php?category=books&book_branch=ece" ||  $current_uri == "items_list.php?category=books&book_branch=ee" ||  $current_uri == "items_list.php?category=books&book_branch=chem" ||  $current_uri == "items_list.php?category=books&book_branch=mechprod" ||  $current_uri == "items_list.php?category=books&book_branch=civ" ||  $current_uri == "items_list.php?category=books&book_branch=biot" ||  $current_uri == "items_list.php?category=books" )
                 {echo " active' ";}
               else{echo "' ";}
              ?> >
-            <a href="#" class="dropdown-toggle js-activated">Books<b class="caret"></b></a>
+            Books<b class="caret"></b>
             <ul class="dropdown-menu" >
-              <li><a href='items_list.php?category=books&book_sem=1'>Freshman year</a></li>
+              <li><a    href='items_list.php?category=books&book_branch=civ'>Freshman year</a></li>
              <li><a href='items_list.php?category=books&book_branch=cseit' >Computer Science/Information Technology</a></li>
              <li><a  href='items_list.php?category=books&book_branch=ece' >Electronics and Communications</a></li>
              <li><a    href='items_list.php?category=books&book_branch=ee'>Electrical</a></li>
@@ -53,7 +57,7 @@
              <li><a     href='items_list.php?category=books&book_branch=biot'>Biotechnology</a></li>
              <li><a    href='items_list.php?category=books'>Books for the Beyond</a></li>
            </ul>
-          </li>
+          </li></a>
 
         <li <?php if($current_uri == "items_list.php?category=bikes"){echo "class='active'";} ?> ><a href='items_list.php?category=bikes'>Bikes</a></li>
         <li <?php if($current_uri == "items_list.php?category=misc"){echo "class='active'";} ?> ><a href='items_list.php?category=misc'>Misc</a></li>
@@ -64,10 +68,10 @@
       <ul class="nav navbar-nav navbar-right">       
 
          <li class="dropdown">
-            <a href="#" class="dropdown-toggle js-activated"><span class="navbar-text" style="margin: 0;">Welcome,</span><?php echo $_SESSION["user_name"]; ?><b class="caret"></b></a>
+            <a href="#" class="dropdown-toggle js-activated">log<?php echo $_SESSION["user_name"]; ?><b class="caret"></b></a>
 
             <ul class="dropdown-menu dropdown-menu-left">
-               <li><a  class="c"   href='logout.php'>Logout</a></li>
+               <li><a href='logout.php'>Logout</a></li>
             </ul></li>
 
         <li <?php if($current_uri == "cart.php"){echo "class='active'";} ?> ><a href='cart.php'><span class="glyphicon glyphicon-shopping-cart"></span>Cart<span class="pull-right col-sm-1"><?php echo $_SESSION["cart_count"]; ?></span></a></li>
