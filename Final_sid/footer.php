d02d069fd2f71e9d4602c5ef4682d7403893bd9e
Ghost151998 <lucky.barhi@gmail.com>
2018-03-28 21:56:15 +0530

    Add files via upload

--- Final_sid/footer.php
+++ Final_sid/footer.php
@@ -1,5 +1,5 @@
-<div class="jumbotron" id="footer" style="margin: 0;margin-top: 6%;position:;bottom: 0;background-color:#222222; color:#777;">
+<div class="jumbotron" id="footer" style="width: 100%; margin: 0;margin-top: 6%;position:absolute;bottom: 0;background-color:#222222; color:#777;">
   <div class="container text-center">
-    <h2 >contact about</h2>      
+    <span style="font-size: 25px;">contact about</span>      
   </div>
 </div>
